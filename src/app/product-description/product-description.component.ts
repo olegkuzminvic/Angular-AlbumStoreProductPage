258f2997800b6a02a8cf0db5a069be841a550bcc
Oleh Kuzmin <oleh.kuzmin@esentri.com>
2018-03-26 04:26:07 +0200

    Part 2
    Nice part

---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,16 @@
+///<reference path="../../../node_modules/@angular/core/src/metadata/directives.d.ts"/>
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
